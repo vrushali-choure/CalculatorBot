816f200fef017cdc16bf881312b2a1a15d8f4631
Kartikbariya7 <36204343+Kartikbariya7@users.noreply.github.com>
2018-02-16 15:16:32 +0530

    a

--- Calculator/Dialogs/RootDialog.cs
+++ Calculator/Dialogs/RootDialog.cs
@@ -22,7 +22,7 @@ namespace Calculator.Dialogs
         {
             // var activity = await result as Activity;
 
-            await context.PostAsync("Do you want to add or mul or div or sub ");
+            await context.PostAsync("Do you want to add or mul or div or  ");
              // sample
 
             // calculate something for us to return
